3f222a345375cd8618b010a997034bb93a188c48
Ghoste <El7ghost@gmail.com>
2026-04-30 16:56:06 +0000

    new animations

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //thinking about it , will add it later...//
+
+const faders = document.querySelectorAll('.fade-in');
+
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if(entry.isIntersecting){
+            entry.target.classList.add('show');
+        }
+    });
+});
+
+faders.forEach(el => observer.observe(el));
